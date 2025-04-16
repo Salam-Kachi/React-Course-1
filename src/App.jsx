7179c41817d2359028c68373945a528490aee709
salam <salamkharagoli@gmail.com>
2025-04-16 13:45:49 +0300

    11 - Event on Click on Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
 import "./App.css";
+import Button from "./components/Button";
 import Form from "./components/Form";
 import Header from "./components/header/Header";
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 const App = () => {
+  // function sayHello() {
+  //   console.log("Hello, World!");
+  // }
+  const sayHello = (name) => {
+    console.log(`Hello ${name}!`);
+  };
+
   return (
     <>
-      <Header />
-      <Form />
+      {/* <Header />
+      <Form /> */}
+      <button type="button" onClick={(e) => sayHello("Karim")}>
+        Click Me
+      </button>
     </>
   );
 };
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = ({ title }) => {
+  return (
+    <div style={{ backgroundColor: "black", color: "white" }}>{title}</div>
+  );
+};
+
+export default Button;
