e67ce43b98314a13b557756e61fc70419352dc34
salam <salamkharagoli@gmail.com>
2025-04-16 15:04:37 +0300

    16-4-2025 User state lec-15

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Button from "./components/Button";
 import Form from "./components/Form";
 import Header from "./components/header/Header";
+import { useState } from "react";
 
 const App = () => {
   // function sayHello() {
@@ -12,13 +13,36 @@ const App = () => {
   };
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#282c34",
+      }}
+    >
       {/* <Header />
       <Form /> */}
-      <button type="button" onClick={(e) => sayHello("Karim")}>
-        Click Me
-      </button>
-    </>
+      {/* <Button
+        onClick={() => {
+          console.log("Click Me Button");
+        }}
+      >
+        <span>Click Me</span>
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("Click Me Again Button");
+        }}
+      >
+        <span>Click Me Again</span>
+      </Button> */}
+      <h1>Hello React</h1>
+      const [count, setCount] = useState(0)
+      <button onClick={() => setCount(count + 1)}>Increament</button>
+      <p>You clicked {count} times</p>
+    </div>
   );
 };
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-const Button = ({ title }) => {
+const Button = ({ onClick, children }) => {
   return (
-    <div style={{ backgroundColor: "black", color: "white" }}>{title}</div>
+    <button
+      type="button"
+      style={{
+        backgroundColor: "chocolate",
+        color: "white",
+        padding: "10px",
+        borderRadius: "5px",
+        margin: "10px",
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 };
 
