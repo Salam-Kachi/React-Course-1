2ac1424e9774d54dec11fa2a604828ef26c4ed67
Salam <markeitng1972@yahoo.com>
2025-04-17 08:03:23 +0300

    React Course lesson 18 - unfinished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState({ username: "", password: "" });
   return (
     <div
       style={{
+        width: "100%",
         display: "flex",
         justifyContent: "center",
         flexDirection: "column",
@@ -14,22 +15,45 @@ const App = () => {
       }}
     >
       <h1>Hello From React</h1>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value);
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (input.username && input.password) {
+            alert(`Username: ${input.username}, Password: ${input.password}`);
+          } else {
+            alert("Please fill in both fields.");
+          }
         }}
-        style={{
-          padding: "10px",
-          fontSize: "16px",
-          borderRadius: "5px",
-          border: "none",
-          marginBottom: "20px",
-        }}
-      />
-      <h1>Your Name is : {input} </h1>
+      >
+        <div>
+          <label htmlFor="username">User Name:</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter your name"
+            value={input.username}
+            onChange={(e) => {
+              setInput(...input, (input.username = e.target.value));
+            }}
+          />
+        </div>
+        <div style={{ marginTop: "10px" }}>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+            value={input.password}
+            onChange={(e) => {
+              setInput(...input, (input.password = e.target.value));
+            }}
+            style={{ marginLeft: "10px" }}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
