ad70c505febfe862a9717c917e6b743a6299d9c9
salam <salamkharagoli@gmail.com>
2025-04-17 12:02:46 +0300

    React Course - Lesson 20 - Amend make table for the Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [fetchedData, setFetchedData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const getData = async () => {
       try {
         const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
         const data = await res.json();
         setFetchedData(data);
       } catch (error) {
-        console.log(error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
     getData();
   }, []);
-  // console.log(fetchedData);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
   return (
     <>
       <h1>Hello From React</h1>
-      {JSON.stringify(fetchedData)}
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fetchedData.map((item) => (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
