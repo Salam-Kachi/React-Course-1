a19c73cd0d89a539728fe7e49bf32ff62f5e1652
salam <salamkharagoli@gmail.com>
2025-04-17 11:24:33 +0300

    React Course - lesson 20 - UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,26 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
-  const [input, setInput] = useState({ username: "", password: "" });
+  const [fetchedData, setFetchedData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+        const data = await res.json();
+        setFetchedData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+  // console.log(fetchedData);
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "lightblue",
-      }}
-    >
+    <>
       <h1>Hello From React</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (input.username && input.password) {
-            alert(`Username: ${input.username}, Password: ${input.password}`);
-          } else {
-            alert("Please fill in both fields.");
-          }
-        }}
-      >
-        <div>
-          <label htmlFor="username">User Name:</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Enter your name"
-            value={input.username}
-            onChange={(e) => {
-              setInput({ ...input, username: e.target.value });
-            }}
-          />
-        </div>
-        <div style={{ marginTop: "10px" }}>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Enter your password"
-            value={input.password}
-            onChange={(e) => {
-              setInput({ ...input, password: e.target.value });
-            }}
-            style={{ marginLeft: "10px" }}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      {JSON.stringify(fetchedData)}
+    </>
   );
 };
 
